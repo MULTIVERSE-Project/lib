97eef8c12b0995c5a2d32f23c3cf3642bb8e66c6
Kot <kotyarishka@gmail.com>
2022-02-19 13:38:22 +0200

    some adds tp language file

--- lua/mvp/languages/en.lua
+++ lua/mvp/languages/en.lua
@@ -15,6 +15,12 @@ lang['config#Main'] = 'Main'
 lang['config#Other'] = 'Other'
 
 /*
+    General use
+*/
+
+lang['close'] = 'Close'
+
+/*
     !mvp menu
 */
 lang['config#MenuConfig'] = 'Config'
@@ -37,5 +43,18 @@ lang['config#ModulesAuthor'] = 'Author %s'
 
 lang['config#Permissions'] = 'List of CAMI permissions registered by base and modules'
 lang['config#PermissionsDefaultAccess'] = 'Default access %s'
+
+/*
+    Thank you screen
+    Please, if you translating this, don't change our message to server owners.
+*/
+lang['thx#FirstLine'] = 'Thank you very much'
+lang['thx#SecondLine'] = 'for using our scripts'
+lang['thx#MainText'] = [[At MULTIVERSE Project we want to make only unique, interesting and cool content, and you've ALREADY become part of our small community. Thank you for supporting and using our work on your server!
+
+This message will only appear once, and only to the SuperAdmins, we won't disturb your players.
+
+If someone recommended you to install our base and a couple of modules for it and you don't know yet that we have a discord, please come and take a look :)]]
+lang['thx#DiscordURL'] = 'Open link to Discord server'
 
 mvp.language.AddTranslation('en', lang)
---
+++ lua/mvp/languages/ru.lua
@@ -0,0 +1,60 @@
+local lang = {}
+
+/*
+    Config descriptions
+*/
+
+lang['#tagDescription'] = 'Префикс, который будет использовать база'
+lang['#menuCommandDescription'] = 'Команда, которая откроет это меню'
+lang['#configPopupsDescription'] = 'Определяет, будут ли уведомления об изменении конфига показываться всем или только админам'
+
+/*
+    Config categories
+*/
+lang['config#Main'] = 'Основное'
+lang['config#Other'] = 'Другое'
+
+/*
+    General use
+*/
+
+lang['close'] = 'Закрыть'
+
+/*
+    !mvp menu
+*/
+lang['config#MenuConfig'] = 'Конфиг'
+lang['config#MenuModules'] = 'Модули'
+lang['config#MenuPermissions'] = 'Права'
+
+/*
+    !mvp menu strings
+*/
+lang['config#Configuration'] = 'Конфигурация для модулей и базы'
+
+lang['config#Modules'] = 'Список всех загружееных модулей'
+lang['config#ModulesRefresh'] = 'Проверить наличие обновлений'
+lang['config#ModulesFetching'] = 'Проверяем наличие обновлений'
+lang['config#ModulesUpToDate'] = 'Последняя версия!'
+lang['config#ModulesNeedsUpdate'] = 'Необходимо обновление!'
+lang['config#ModulesNoInformation'] = 'Нет информации о модуле!'
+lang['config#ModulesNewVersion'] = 'Новая версия - %s'
+lang['config#ModulesAuthor'] = 'Автор %s'
+
+lang['config#Permissions'] = 'Список прав CAMI, которые были зарегстрированы базой или модулями'
+lang['config#PermissionsDefaultAccess'] = 'Стандартный доступ %s'
+
+/*
+    Thank you screen
+    Please, if you translating this, don't change our message to server owners.
+*/
+lang['thx#FirstLine'] = 'Огромное вам спасибо'
+lang['thx#SecondLine'] = 'за использование наших скриптов'
+lang['thx#MainText'] = [[В MULTIVERSE Project мы хотим делать только уникальный, интересный и крутой контент, и вы уже стали частью нашего небольшого сообщества. Спасибо за поддержку и использование нашей работы на вашем сервере!
+
+Это сообщение появится только один раз и только для СуперАдминов, мы не будем беспокоить ваших игроков.
+
+Если кто-то посоветовал вам установить нашу базу и пару модулей для нее, и вы еще не знаете, что у нас есть дискорд, пожалуйста, зайдите и посмотрите :)]]
+lang['thx#DiscordURL'] = 'Открыть наш Дискорд'
+
+mvp.language.AddTranslation('ru', lang)
