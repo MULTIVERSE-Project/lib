4c27d25fc0abc7b7d8257bd265aa79ed40c8ac26
Danylo <44447172+Kotyarishka@users.noreply.github.com>
2022-08-12 13:25:05 +0300

    fa-icons.json

--- lua/mvp/core/config/language.lua
+++
@@ -1,5 +0,0 @@
-mvp.config.Add('language', 'en', '#languageDescription', nil, {
-    category = 'config#Main',
-    type = 'select',
-    from = mvp.language.GetLoadedLanguages
-}, false, false)
--- lua/mvp/core/config/main.lua
+++
@@ -1,4 +0,0 @@
-mvp.config.Add('mvpTag', 'MVP', '#tagDescription', nil, {category = 'config#Main', type = 'string'}, false, false) 
-mvp.config.Add('mvpCommand', '!mvp', '#menuCommandDescription', nil, {category = 'config#Main', type = 'string'}, false, false)
-mvp.config.Add('canPlayersSeeConfigPopups', false, '#configPopupsDescription', nil, {category = 'config#Main', type = 'bool'}, false, false) 
-
