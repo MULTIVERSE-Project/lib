0cc702dcc071ca008da7bc0d65f626413d4d423e
Kot <ketimox.info@gmail.com>
2022-08-17 11:24:27 +0300

    mvp loader

---
+++ lua/mvp/core/sh_loader.lua
@@ -0,0 +1,90 @@
+--- Loader for the library. 
+-- @module mvp.loader
+
+mvp = mvp or {}
+
+mvp.loader = mvp.loader or {}
+mvp.loader.relativePath = 'mvp/'
+
+local function rgb(r, g, b, a)
+    return Color(r, g, b, a or 255)
+end
+
+local function p(...)
+    if mvp.utils and mvp.utils.Print then
+        return mvp.utils.Print(...)
+    end
+
+    MsgC(rgb(0, 168, 255), '[MVP]', rgb(225, 177, 44), '[Loader] ', rgb(255, 255, 255), ...)
+    MsgC('\n')
+end
+
+--- Sends and loads client files.
+-- Should be called only on shared!
+-- @realm shared
+-- @param path Path to the file to load.
+function mvp.loader.LoadCLFile(path)
+    if SERVER then
+        AddCSLuaFile(path)
+    else
+        include(path)
+    end
+    
+    p('Loaded ', rgb(68, 189, 50), 'CL', rgb(255, 255, 255), ' file: ', path)
+end
+
+
+--- Loads server files.
+-- It won't do anything if called on client.
+-- @realm shared
+-- @param path Path to the file to load.
+function mvp.loader.LoadSVFile(path)
+    if SERVER then
+        include(path)
+    end
+
+    p('Loaded ', rgb(0, 151, 230), 'SV', rgb(255, 255, 255), ' file: ', path)
+end
+
+--- Send and loads client files.
+-- Should be called only on shared!
+-- @realm shared
+-- @param path Path to the file to load.
+function mvp.loader.LoadSHFile(path)
+    if SERVER then
+        AddCSLuaFile(path)
+    end
+    include(path)
+
+    p('Loaded ', rgb(140, 122, 230), 'SH', rgb(255, 255, 255), ' file: ', path)
+end
+
+--- Send and loads files.
+-- This function will determine if the file is a client file, server file or shared file and call the appropriate function.
+-- @realm shared
+-- @param path Path to the file to load.
+function mvp.loader.LoadFile(path)
+    if path:find('cl_') then
+        mvp.loader.LoadCLFile(path)
+    elseif path:find('sv_') then
+        mvp.loader.LoadSVFile(path)
+    else
+        mvp.loader.LoadSHFile(path)
+    end
+end
+
+--- Loads all files in a directory.
+-- @realm shared
+-- @param path Path to the directory to load.
+function mvp.loader.LoadFolder(path)
+    path = mvp.loader.relativePath .. path
+    local files, folders = file.Find(path .. '/*', 'LUA')
+
+    for _, file in pairs(files) do
+        mvp.loader.LoadFile(path .. '/' .. file)
+    end
+
+    for _, folder in pairs(folders) do
+        mvp.loader.LoadFolder(path .. '/' .. folder)
+    end
+end
