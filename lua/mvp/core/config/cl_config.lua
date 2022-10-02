c47f56779911afa47a26357cf3f91aa528c43ceb
Kot <ketimox.info@gmail.com>
2022-10-02 19:01:51 +0300

    permissions control + some hooks + console commands

---
+++ lua/mvp/core/config/cl_config.lua
@@ -0,0 +1,29 @@
+--- @module mvp.config
+mvp.config = mvp.config or {}
+
+net.Receive('mvpConfigList', function()
+    local data = net.ReadTable()
+
+    for k, v in pairs(data) do
+        if mvp.config.stored[k] then
+            mvp.config.stored[k].value = v
+        end
+    end
+
+    hook.Run('RecivedMVPConfigs')
+end)
+
+net.Receive('mvpConfigSet', function()
+    local key = net.ReadString()
+    local value = net.ReadType()
+
+    local config = mvp.config.stored[key]
+
+    if config then
+        if config.callback then
+            config.callback(config.value, value)
+        end        
+
+        config.value = value
+    end
+end)
--- lua/mvp/sv_init.lua
+++ lua/mvp/sv_init.lua
@@ -4,3 +4,26 @@ mvp = mvp or {}
 -- Send essential files to client first
 AddCSLuaFile('mvp/core/sh_loader.lua')
 include('mvp/core/sh_loader.lua')
+
+util.AddNetworkString('mvpPlayerReady')
+util.AddNetworkString('mvpMenu')
+
+-- Player ready
+net.Receive('mvpPlayerReady', function(len, ply)
+    if ply.mvpReady then return end
+
+    -- Set player ready
+    ply.mvpReady = true
+    hook.Run('mvp.hooks.PlayerReady', ply)
+end)
+
+-- handle menu command
+hook.Add('PlayerSay', 'mvp.hooks.OpenConfig', function(ply, text)      
+    if string.Trim(string.lower(text)) ~= mvp.config.Get('chatCommand', '!mvp') then return end
+    if not mvp.permissions.Check(ply, 'mvp.admin') then return end
+
+    net.Start('mvpMenu')
+    net.Send(ply)
+
+    return true
+end)
